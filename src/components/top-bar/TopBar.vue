7ffe9ab7573626477d71a2ac27d23a311d3a25b5
Julio Filho <juliorodriguesfilho@gmail.com>
2024-09-19 19:13:12 -0300

    feat: updated to component

---
+++ src/components/top-bar/TopBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="topbar">
+    <div class="info-container">
+      <img src="../../assets/images/profile.png" alt="profile" class="image" />
+      <div class="info">
+        <span class="top"> Eduardo Pereira da Costa </span>
+        <span class="bottom"> Front-end Developer </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FATopBar'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../assets/styles/theme.styl'
+.topbar
+ display flex
+ flex-direction row
+ height fit-content
+ width 100%
+ background-color #fff
+ .info-container
+   display flex
+   flex-direction row
+   margin-left auto
+   padding 2.5vh 5vh 4vh
+   .image
+     border-radius 25px
+     margin-right 0.5vw
+     cursor pointer
+   .info
+     display flex
+     flex-direction column
+     .top
+       font-family $td-semi-bold
+       color $td-title
+       font-size 18px
+       margin-bottom -7px
+     .bottom
+       font-family $td-semi-bold
+       color $td-blue
+       font-size 14px
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -34,24 +34,18 @@
       </div>
     </div>
     <div class="right-container">
-      <div class="topbar">
-        <div class="info-container">
-          <img src="../assets/images/profile.png" alt="profile" class="image" />
-          <div class="info">
-            <span class="top"> Eduardo Pereira da Costa </span>
-            <span class="bottom"> Front-end Developer </span>
-          </div>
-        </div>
-      </div>
+      <FATopBar />
     </div>
   </div>
 </template>
 <script>
 import FAActionItem from '../components/action-item/ActionItem.vue'
+import FATopBar from '../components/top-bar/TopBar.vue'
 
 export default {
   components: {
-    FAActionItem
+    FAActionItem,
+    FATopBar
   },
   data() {
     return {
@@ -100,31 +94,4 @@ export default {
   flex-direction column
   width 93%
   height 100%
-  .topbar
-    display flex
-    flex-direction row
-    height fit-content
-    width 100%
-    background-color #fff
-    .info-container
-      display flex
-      flex-direction row
-      margin-left auto
-      padding 2.5vh 5vh 4vh
-      .image
-        border-radius 25px
-        margin-right 0.5vw
-        cursor pointer
-      .info
-        display flex
-        flex-direction column
-        .top
-          font-family $td-semi-bold
-          color $td-title
-          font-size 18px
-          margin-bottom -7px
-        .bottom
-          font-family $td-semi-bold
-          color $td-blue
-          font-size 14px
 </style>
